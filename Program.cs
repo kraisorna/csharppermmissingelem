4aaa752cd5a6339512a11713bae02694e7f2f8af
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 17:51:55 +0700

    First local commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPermMissingElem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] A = { 2,3,1,5 };
+            Console.WriteLine(s.solution(A));
+            Console.ReadLine();
+        }
+    }
+
+    class Solution
+    {
+
+        public int solution(int[] A)
+        {
+            if (A.Length == 0)
+                return 1;
+            var result = A.Length + 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                result = result ^ (i + 1);
+                result = result ^ A[i];
+            }
+            return result;
+        }
+    }
+}
